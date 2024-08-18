a9a2051e88a53bfc07a666b106f853cc443c43da
Peter114544 <138435607+Peter114544@users.noreply.github.com>
2024-08-18 18:03:25 +0800

    1

---
+++ java5/src/ExtendedAssignmentOperator.java
@@ -0,0 +1,10 @@
+public class ExtendedAssignmentOperator {
+    public static void main(String[] args) {
+        //+=  -=  *=  /=  %=
+        int a =10;
+        int b =20;
+        a+=b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
