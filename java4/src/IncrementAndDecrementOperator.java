138d8e42d337fac85eb500203cf06695294d80bb
Peter114544 <138435607+Peter114544@users.noreply.github.com>
2024-08-18 15:43:21 +0800

    111
    www

---
+++ java4/src/IncrementAndDecrementOperator.java
@@ -0,0 +1,15 @@
+public class IncrementAndDecrementOperator {
+    public static void main(String[] args) {
+        int a = 10;//++/--
+        a++;
+        System.out.println(a);//11
+        a--;
+        System.out.println(a);//10
+        ++a;
+        System.out.println(a);//11
+        --a;
+        System.out.println(a);//10
+
+
+    }
+}
