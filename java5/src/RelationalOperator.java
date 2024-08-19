59455539805d01db291ade70b0dbca086d9d3aea
Peter114544 <138435607+Peter114544@users.noreply.github.com>
2024-08-19 14:39:55 +0800

    1

---
+++ java5/src/RelationalOperator.java
@@ -0,0 +1,11 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        int c = 20;
+        System.out.println(a == b);//true
+        System.out.println(a == c); //false
+
+    }
+}
