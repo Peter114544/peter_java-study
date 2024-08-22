4ba99146f935eba6beaf030f6acfe6390b8c6830
Peter114544 <138435607+Peter114544@users.noreply.github.com>
2024-08-22 22:19:06 +0800

    1

---
+++ java9/src/ForLoopExercises_OutputData.java
@@ -0,0 +1,12 @@
+public class ForLoopExercises_OutputData {
+    public static void main(String[] args) {
+        int number = 1;
+        for (int i = 1; i <=5; i++) {
+            System.out.println(number++);
+        }
+        int number2 = 5;
+        for (int b = 1; b <=5; b++) {
+            System.out.println(number2--);
+        }
+    }
+}
---
+++ java9/src/SumForLoopPractice.java
@@ -0,0 +1,9 @@
+public class SumForLoopPractice{
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 101; i++) {
+            a = a + i;
+        }
+        System.out.println(a);
+    }
+}
