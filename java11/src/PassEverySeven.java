476362c6e5717c32558d246fe89c5c423025826d
Peter114544 <138435607+Peter114544@users.noreply.github.com>
2024-08-24 21:44:24 +0800

    ^-^

---
+++ java11/src/PassEverySeven.java
@@ -0,0 +1,10 @@
+public class PassEverySeven {
+    public static void main(String[] args) {
+        for (int i =0;i<=100;i++){
+            if(i%7!=0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
